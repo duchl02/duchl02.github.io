ab09092042f3e6ee70d9695198c36a10fc9e4a96
“duchl02” <duchl0202@gmail.com>
2022-06-02 21:53:29 +0700

    them mor chut

--- assets/results/test/main.js
+++ assets/results/test/main.js
@@ -85,21 +85,177 @@
 // },0)
 // console.log(array)
 
-function run() {
-    let a = document.querySelector('#data').value
-    let checked= true
+// function run() {
+//     let a = document.querySelector('#data').value
+//     let checked= true
 
 
-    for (let i = 2; i < a; ++i) {
-        if (a % i == 0){
-            checked = false
-            break;
-        }
-    }
-    if (checked == true){
-        console.log(a + " là số nguyên tố ");
-    }
-    else{
-        console.log(a + " không phải là số nguyên tố ");
-    }
-}
+//     for (let i = 2; i < a; ++i) {
+//         if (a % i == 0){
+//             checked = false
+//             break;
+//         }
+//     }
+//     if (checked == true){
+//         console.log(a + " là số nguyên tố ");
+//     }
+//     else{
+//         console.log(a + " không phải là số nguyên tố ");
+//     }
+// }
+
+// function run() {
+//     let a1 = 0
+//     let a = document.getElementsByTagName('div')
+//     for (let i = 0; i < a.length; i++) {
+//         if (i % 2 !== 0) {
+            
+//             a1 = a1 + i
+//             console.log(a1)
+//         } else{
+//             a[i].style.background='yellow'
+//         }
+//     }
+// }
+
+// var menu = document.querySelectorAll('#dropdown > li')
+// for (let i = 0; i < menu.length; i++) {
+//     menu[i].addEventListener('click', function(){
+//         var menulist = document.querySelectorAll('#dropdown > li > ul')
+//         for (let j = 0; j < menulist.length; j++){
+//             menulist[j].style.display = 'none'
+//         }
+//         this.children[1].style.display = 'block'
+//     })
+// }
+
+// var anh1= '../CloneYoutube/assets/img/tituc.webp'
+// var anh2 = '../CloneYoutube/assets/img/music-channel.jpeg'
+// var img = document.querySelector('#img')
+// document.querySelector('#anhdep1').onclick = function(){
+//     img.src = anh1
+// }
+// document.querySelector('#anhdep2').onclick = function(){
+//     img.src = anh2
+// }
+
+// function run(){
+//     let a = document.querySelector('#showSA')
+//     let text = a.innerHTML;
+//     let category = document.querySelector('#select_avanced')
+
+//     if (text == 'nhan de tim kiem'){
+//         category.style.display = 'table-row',
+//         a.innerHTML='bo tim kiem'
+//     } else if (text == 'bo tim kiem'){
+//         category.style.display = 'none',
+//         a.innerHTML='nhan de tim kiem'
+//     }
+//     return false;
+// }
+
+// let btn = document.querySelectorAll('#button')
+// for (let i = 0; i < btn.length; i++){
+//     btn[i].addEventListener('click', function(){
+//         return this.parentElement.parentElement.remove()
+//     })
+// }
+
+// const s = '123456789'
+// const n = 3
+// let result = []
+// for (let i = 0; i < s.length; i += n){
+//     let ran=''
+//     for (let j = i; j < n + i; j++){
+//         ran= ran + s[j]
+//     }
+//     result.push(ran)
+// }
+// console.log(result)
+
+// function getValue(id){
+//     return document.getElementById(id).value.trim();
+// }
+
+// // Hiển thị lỗi
+// function showError(key, mess){
+//     document.getElementById(key + '_error').innerHTML = mess;
+// }
+
+// function validate()
+// {
+//     var flag = true;
+
+//     // 1 username
+//     var username = getValue('username');
+//     if (username == '' || username.length < 5 || !/^[a-zA-Z0-9]+$/.test(username)){
+//         flag = false;
+//         showError('username', 'Vui lòng kiểm tra lại Username');
+//     }
+
+//     // 2. password
+//     var password = getValue('password');
+//     var repassword = getValue('repassword');
+//     if( password != repassword){
+//         flag = false;
+//         showError('repassword', 'mat khau k khop')
+//     }
+//     if (password == '' || password.length < 8){
+//         flag = false;
+//         showError('password', 'Vui lòng kiểm tra lại Password');
+//     }
+
+//     // 3. Phone
+//     var phone = getValue('phone');
+//     if (phone != '' &&  !/^[0-9]{10}$/.test(phone) ){
+//         flag = false;
+//         showError('phone', 'Vui lòng kiểm tra lại Phone');
+//     }
+
+//     // 4. Email
+//     var email = getValue('email');
+//     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//     if (!mailformat.test(email)){
+//         flag = false;
+
+//         showError('email', 'Vui lòng kiểm tra lại Email');
+//     }
+
+//     return flag;
+// }
+
+
+
+// function firstQuestion() {
+// var curdate = new Date()
+// var year = curdate.getFullYear()
+// var month = curdate.getMilliseconds()
+// var day = curdate.getMinutes()
+// var a =  document.querySelector('#time');
+// var tim = curdate.getSeconds()
+//     a.innerHTML= day +'/ '+ tim
+//     a.style.width="100px"
+//     a.style.height="100px"
+//     a.style.background='black'
+// }
+// setInterval(firstQuestion)
+// console.log(firstQuestion.construction)
+
+// console.log(Math.floor(Math.random() * 10))
+// $('#button-dao-nguoc').click(function() {
+//     var oj=$('div')
+//     for (let i=0; i<oj.length; i++) {
+//         if($(oj[i]).hasClass('yellow')){
+//             $(oj[i]).removeClass('yellow').addClass('red')
+//         }
+//         else {
+//             $(oj[i]).removeClass('red').addClass('yellow')
+//         }
+//     }
+// })
+
+$('#button-dao-nguoc').click(function(){
+    var content = '<h1>hello</h1>'
+    $('#container').html(content)
+})
+
