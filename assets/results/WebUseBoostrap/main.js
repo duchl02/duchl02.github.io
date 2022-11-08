0d4d95b68231a98842347db9deb51fbffe3b7fda
“duchl02” <duc1503honglinh@gmail.com>
2022-11-08 11:28:07 +0700

    commit 08/11

--- assets/results/WebUseBoostrap/main.js
+++ assets/results/WebUseBoostrap/main.js
@@ -1,19 +1,26 @@
-$(window).scroll(function() {
-    $(".slideScroll").each(function(){
-      var pos = $(this).offset().top;
-      var winTop = $(window).scrollTop();
-      console.log(winTop);
-      if (pos < winTop + 600) {
-        $(this).addClass("slide");
-      }
-    });
+$(window).scroll(function () {
+  $(".slideScroll").each(function () {
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
 });
-$('.icon-srtop').on('click', function(){
-    $(window).scrollTop(0)
-})
-$(window).ready(function(){
-    $('.modal').modal('show');
-    $('.btn-modal').on('click', function(){
-        $('.modal').modal('show');
-    })
-})
+$(".icon-srtop").on("click", function () {
+  $(window).scrollTop(0);
+});
+$(window).ready(function () {
+  $(".modal").modal("show");
+  $(".btn-modal").on("click", function () {
+    $(".modal").modal("show");
+  });
+  $(window).scroll(function () {
+    let height = document.documentElement.scrollHeight;
+    let topheight = document.documentElement.scrollTop;
+    let client_height = document.documentElement.clientHeight;
+
+    let value = Math.floor((topheight / (height - client_height)) * 100);
+    $('.value').css('width', value + '%');
+  });
+});
